19b4795553bc15708c0362e5dec59ba8d46b04a5
Gregory Nelson Esguerra <gregorye@ggsinc.com>
2024-11-22 14:30:05 +0800

    Add project files.

---
+++ WpfDataGridCheckbox/DataLayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace WpfDataGridCheckbox
+{
+    public class DataLayer
+    {
+        public static DataTable GetProduct()
+        {
+            DataSet ds = new DataSet();
+            string query = "Select * from Products;";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["products"].ConnectionString.ToString()))
+                {
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    conn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(ds);
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ds.Tables[0];
+        }
+
+        public static int UpdateProductDiscontinue(bool value, string product_name)
+        {
+            int result = 0;
+            string query = String.Format("Update Products set discontinue = '{0}' where productname = '{1}' ;", value, product_name);
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["products"].ConnectionString.ToString()))
+                {
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    conn.Open();
+                    result = cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result;
+        }
+    }
+}
---
+++ WpfDataGridCheckbox/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Collections;
+
+namespace WpfDataGridCheckbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgProducts.ItemsSource = DataLayer.GetProduct().AsDataView();
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var row = GetDataGridRows(dgProducts);
+
+            if (((CheckBox)sender).IsChecked == true)
+            {
+                SetCheckbox(row, true);
+            }
+            else
+            {
+                SetCheckbox(row, false);
+            }
+        }
+
+        //individual checking
+        private void chkDiscontinue_Click(object sender, RoutedEventArgs e)
+        {
+            object a = e.Source;
+            CheckBox chk = (CheckBox)sender;
+
+            DataGridRow row = FindAncestor<DataGridRow>(chk);
+            if (row != null)
+            {
+                DataRowView rv = (DataRowView)row.Item;
+
+                //LINQ or Database Method to Update Product discontinue status
+                DataLayer.UpdateProductDiscontinue((bool)chk.IsChecked, rv["ProductName"].ToString());
+            }
+        }
+
+        private void SetCheckbox(IEnumerable<DataGridRow> row, bool value)
+        {
+            //loop through datagrid rows
+            foreach (DataGridRow r in row)
+            {
+                DataRowView rv = (DataRowView)r.Item;
+                foreach (DataGridColumn column in dgProducts.Columns)
+                {
+                    if (column.GetType().Equals(typeof(DataGridTemplateColumn)))
+                    {
+                        rv.Row["Discontinue"] = value;
+
+                        //LINQ or Database Method to Update Product discontinue status
+                        DataLayer.UpdateProductDiscontinue(value, rv.Row["productname"].ToString());
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<DataGridRow> GetDataGridRows(DataGrid grid)
+        {
+            var itemsSource = grid.ItemsSource as IEnumerable;
+            if (null == itemsSource) yield return null;
+            foreach (var item in itemsSource)
+            {
+                var row = grid.ItemContainerGenerator.ContainerFromItem(item) as DataGridRow;
+                if (null != row) yield return row;
+            }
+        }
+
+        public static T FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        {
+            current = VisualTreeHelper.GetParent(current);
+            while (current != null)
+            {
+                if (current is T)
+                {
+                    return (T)current;
+                }
+
+                current = VisualTreeHelper.GetParent(current);
+            };
+            return null;
+        }
+
+        private childItem FindVisualChild<childItem>(DependencyObject obj) where childItem : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem)
+                    return (childItem)child;
+                else
+                {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+
+            return null;
+        }
+    }
+}
